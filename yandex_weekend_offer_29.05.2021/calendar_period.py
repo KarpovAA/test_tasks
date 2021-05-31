8ed516073bfa735d9a897fa94880c3bc30577a08
Karpov.A <karpov_aa@mail.ru>
2021-05-31 16:44:56 +0700

    added calculation of the latest date of the period - get_latest_date_of_period
    added parsing input
    - parse_input

--- yandex_weekend_offer_29.05.2021/calendar_period.py
+++ yandex_weekend_offer_29.05.2021/calendar_period.py
@@ -6,7 +6,7 @@ import calendar
 import datetime as dt
 
 
-def get_end_period(period: str, start_date: dt.datetime, end_date: dt.datetime) -> dt.datetime:
+def get_latest_date_of_period(period: str, start_date: dt.datetime) -> [dt.datetime, None]:
     """
     :param period:  WEEK — неделя с понедельника по воскресенье.
                     MONTH — месяц.
@@ -15,29 +15,50 @@ def get_end_period(period: str, start_date: dt.datetime, end_date: dt.datetime) 
                     REVIEW — периоды, за которые оцениваются достижения сотрудников Яндекса.
                              Летний период длится с 1 апреля по 30 сентября, зимний — с 1 октября по 31 марта.
     :param start_date: Дата начала периода
-    :param end_date: Дата окончания периода
     :return:
     """
     if period == 'WEEK':
-        pass
+        return start_date + dt.timedelta(days=6 - start_date.weekday())     # последний день недели
+
     if period == 'MONTH':
-        pass
+        days_in_month = calendar.monthrange(start_date.year, start_date.month)[1]   # количество дней в месяце
+        return dt.datetime(start_date.year, start_date.month, days_in_month)        # последний день месяца
+
     if period == 'QUARTER':
-        pass
+        n_month = (start_date.month // 3 + 1) * 3                         # последний месяц текущего квартала
+        days_in_month = calendar.monthrange(start_date.year, n_month)[1]  # кол-во дней в последнем месяце квартала
+        return dt.datetime(start_date.year, n_month, days_in_month)       # последний день квартала
+
     if period == 'YEAR':
-        pass
+        return dt.datetime(start_date.year, 12, 31)                       # последний день текущего года
+
     if period == 'REVIEW':
-        pass
+        if 4 <= start_date.month <= 9:
+            month = 9
+            year = start_date.year
+        else:
+            month = 3
+            year = start_date.year + 1
+        days_in_month = calendar.monthrange(year, month)[1]
+        return dt.datetime(year, month, days_in_month)                    # последний день ревью
+
     return None
 
 
 def parse_input():
-    pass
+    period = sys.stdin.readline().strip()
+    start_period, end_period = sys.stdin.readline().strip().split(' ')
+    return period, dt.datetime.strptime(start_period, '%Y-%m-%d'), dt.datetime.strptime(end_period, '%Y-%m-%d')
 
 
 def main():
-    parse_input()
-    get_end_period('WEEK', dt.datetime.now(), dt.datetime.now())
+    period, start_period, end_period = parse_input()
+    print('Окончание периода: ', str(get_latest_date_of_period(period, start_period).date()))
 
 
-main()
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Execution is interrupted by pressing Control+C')
+
