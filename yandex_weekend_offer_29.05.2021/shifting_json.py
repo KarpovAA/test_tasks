38a20d7998aa0c677364280022f4dcf5284d4c80
a.karpov <karpov_aa@mail.ru>
2021-05-31 22:58:11 +0700

    blank shifting_json.py

---
+++ yandex_weekend_offer_29.05.2021/shifting_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Перекладывание JSON
+
+import sys
+
+
+def parse_input():
+    n_fids = sys.stdin.readline().strip()
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Execution is interrupted by pressing Control+C')
