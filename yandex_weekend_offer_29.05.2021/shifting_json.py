037238fca50148e2c99d50e2a64a21c090202b05
Karpov.A <karpov_aa@mail.ru>
2021-06-01 15:22:04 +0700

    first work version shifting_json.py

--- yandex_weekend_offer_29.05.2021/shifting_json.py
+++ yandex_weekend_offer_29.05.2021/shifting_json.py
@@ -3,15 +3,31 @@
 # Перекладывание JSON
 
 import sys
+import json
 
 
-def parse_input():
-    n_fids = sys.stdin.readline().strip()
-    pass
+def merge_fids(fids):
+    comb_fids = list()
+    for i in fids:
+        for j in i['offers']:
+            comb_fids.append(j)
+    return sorted(comb_fids, key=lambda x: (x['price'], x['offer_id']))
+
+
+def parse_input() -> [dict]:
+    n_fids = int(sys.stdin.readline().strip())
+    fids = []
+    for _ in range(n_fids):
+        fids.append(json.loads(sys.stdin.readline().strip()))
+    return fids
 
 
 def main():
-    pass
+    # '{"offers": [{"offer_id": "offer1", "market_sku": 10846332, "price": 499},
+    #              {"offer_id": "offer2", "market_sku": 682644, "price": 499}]}'
+    # '{"offers": [{"offer_id": "offer3", "market_sku": 832784, "price": 14000}]}'
+    fids = parse_input()
+    print(json.dumps({'offers': merge_fids(fids)}))
 
 
 if __name__ == "__main__":
