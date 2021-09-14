6fc8cd99827a155eaa6e21cce0d7ded7e3dc1619
Karpov.A <karpov_aa@mail.ru>
2021-09-14 09:05:31 +0700

    update task1.py (work_version v.1)

--- yandex_cup_/task1.py
+++ yandex_cup_/task1.py
@@ -3,50 +3,77 @@ import json
 from collections import defaultdict
 
 
-orders = defaultdict(defaultdict)
-item = {'id': None, 'count': 0}
-items = list(item)
-
-# data = json.load(sys.stdin
+# data = json.load(sys.stdin)
 data = [
-            {"event_id": 1,
-            "order_id": 1,
-            "item_id": 1,
-            "count": 1,
-            "return_count": 0,
-            "status": "OK"
-            },
-            {
+        {
             "event_id": 2,
+            "order_id": 2,
+            "item_id": 1,
+            "count": 3,
+            "return_count": 1,
+            "status": "OK"
+        },
+        {
+            "event_id": 1,
             "order_id": 1,
             "item_id": 1,
-            "count": 1,
-            "return_count": 0,
-            "status": "CANCEL"
-            },
-            {
-            "event_id": 3,
-            "order_id": 1,
-            "item_id": 2,
-            "count": 1,
+            "count": 2,
             "return_count": 0,
             "status": "OK"
-            }
-        ]
+        },
+        {
+            "event_id": 3,
+            "order_id": 3,
+            "item_id": 1,
+            "count": 2,
+            "return_count": 2,
+            "status": "OK"
+        }
+    ]
+# data = [
+#             {"event_id": 1,
+#             "order_id": 1,
+#             "item_id": 1,
+#             "count": 1,
+#             "return_count": 0,
+#             "status": "OK"
+#             },
+#             {
+#             "event_id": 2,
+#             "order_id": 1,
+#             "item_id": 1,
+#             "count": 1,
+#             "return_count": 0,
+#             "status": "CANCEL"
+#             },
+#             {
+#             "event_id": 3,
+#             "order_id": 2,
+#             "item_id": 2,
+#             "count": 1,
+#             "return_count": 0,
+#             "status": "OK"
+#             }
+#         ]
 
 
-def item_update(order_id, item_id, item_count):
-
-    pass
-
+orders = defaultdict(defaultdict)
 
 for order in data:
-    if order['status'] == 'OK':
-        orders[order['order_id']][order['item_id']] = orders[order['order_id']][order['item_id']] + order['count'] - order['return_count']
+    if order['order_id'] not in orders:
+        orders[order['order_id']] = defaultdict(int)
+        orders[order['order_id']][order['item_id']] = 0
+
+    if order['status'] == 'CANCEL':
+        orders[order['order_id']][order['item_id']] = 0
     else:
-        orders[order['order_id']][order['item_id']] = orders[order['order_id']][order['item_id']] - order['count'] - order['return_count']
-for order_id, order in orders.items():
-    for item_id, item_count in order.items():
-        print('order_id:', order_id, 'item_id:', item_id, 'item_count:', item_count)
-# print(orders)
-# print(orders[1][1])
+        orders[order['order_id']][order['item_id']] += order['count'] - order['return_count']
+    pass
+
+result = list()
+for k, v in orders.items():
+    list_items = [{'id': item_id, 'count': item_count} for item_id, item_count in v.items() if item_count > 0]
+    if list_items:
+        result.append({'id': k, 'items': list_items})
+
+sys.stdout.write(json.dumps(result))
